47dde32f88cf28e7fc4d2b4440bd43fff846af95
Somtochukwu Nkemakolam <sewkito@gmail.com>
2023-05-19 10:01:15 +0100

    Removed some unused images

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,14 +3,6 @@ import { FoodQuotes } from "./localData/Data";
 export { FoodQuotes };
 
 // Images
-
-export { default as slide2 } from "../assets/images/slide2.jpg";
-export { default as slide3 } from "../assets/images/slide3.jpg";
-export { default as slide4 } from "../assets/images/slide4.jpg";
-export { default as slide5 } from "../assets/images/slide5.jpg";
-export { default as slide6 } from "../assets/images/slide6.jpg";
-export { default as slide7 } from "../assets/images/slide7.jpg";
-export { default as slide8 } from "../assets/images/slide8.jpg";
 export { default as slide9 } from "../assets/images/slide9.jpg";
 export { default as chef1 } from "../assets/images/chef1.png";
 export { default as NoFav } from "../assets/images/NoFave.png";
